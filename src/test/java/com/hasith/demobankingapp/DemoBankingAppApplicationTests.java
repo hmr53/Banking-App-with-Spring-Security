39d1f6761c02331374509d1d66ed51b4807015e2
hmr53 <160482647+hmr53@users.noreply.github.com>
2024-03-12 13:08:32 +0530

    Initial Commit.

---
+++ src/test/java/com/hasith/demobankingapp/DemoBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasith.demobankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
